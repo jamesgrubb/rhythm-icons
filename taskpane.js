24afcf3c310bb21032fc6f2aaff722537203f697
jamesgrubb <22652+jamesgrubb@users.noreply.github.com>
2026-04-09 12:35:10 +0100

    UI Updates labe changed and edit icon color

--- taskpane.js
+++ taskpane.js
@@ -529,7 +529,7 @@ Office.onReady(async ({ host }) => {
           // Edit button
           const editBtn = document.createElement("button");
           editBtn.className = "icon-edit-btn";
-          editBtn.innerHTML = `<svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>`;
+          editBtn.innerHTML = `<svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>`;
           editBtn.title = "Edit icon";
           editBtn.addEventListener("click", (e) => {
             e.stopPropagation();
